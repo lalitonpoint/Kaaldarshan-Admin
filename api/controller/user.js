dfa42df9057a0a2692e3ee02cb5c94d1369b9bcd
lalitonpoint <lalit@appsquadz.co>
2025-07-02 15:53:25 +0530

    billing_aws

--- api/controller/user.js
+++ api/controller/user.js
@@ -18,7 +18,6 @@ const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 })
-const { CostExplorerClient, GetCostAndUsageCommand, GetCostForecastCommand } = require("@aws-sdk/client-cost-explorer");
 
 
 
